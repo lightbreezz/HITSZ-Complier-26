35017567e2b18877378a26f96150bb8d1bd0a667
lightbreezz <2371631571@qq.com>
2026-03-31 09:42:44 +0800

    feat: fininsh lab1

--- src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
+++ src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
@@ -1,9 +1,10 @@
 package cn.edu.hitsz.compiler.lexer;
 
-import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 import cn.edu.hitsz.compiler.utils.FileUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 /**
@@ -31,7 +32,7 @@ public class LexicalAnalyzer {
         // TODO: 词法分析前的缓冲区实现
         // 可自由实现各类缓冲区
         // 或直接采用完整读入方法
-        throw new NotImplementedException();
+        sourceCode = FileUtils.readFile(path);
     }
 
     /**
@@ -40,7 +41,67 @@ public class LexicalAnalyzer {
      */
     public void run() {
         // TODO: 自动机实现的词法分析过程
-        throw new NotImplementedException();
+        tokens = new ArrayList<>();
+
+        var index = 0;
+        final var length = sourceCode.length();
+        while (index < length) {
+            final var current = sourceCode.charAt(index);
+
+            if (Character.isWhitespace(current)) {
+                index++;
+                continue;
+            }
+
+            if (isIdentifierStart(current)) {
+                final var start = index;
+                index++;
+                while (index < length && isIdentifierPart(sourceCode.charAt(index))) {
+                    index++;
+                }
+
+                final var lexeme = sourceCode.substring(start, index);
+                if ("int".equals(lexeme) || "return".equals(lexeme)) {
+                    tokens.add(Token.simple(lexeme));
+                } else {
+                    tokens.add(Token.normal("id", lexeme));
+                    if (!symbolTable.has(lexeme)) {
+                        symbolTable.add(lexeme);
+                    }
+                }
+                continue;
+            }
+
+            if (Character.isDigit(current)) {
+                final var start = index;
+                index++;
+                while (index < length && Character.isDigit(sourceCode.charAt(index))) {
+                    index++;
+                }
+
+                tokens.add(Token.normal("IntConst", sourceCode.substring(start, index)));
+                continue;
+            }
+
+            switch (current) {
+                case '=' -> tokens.add(Token.simple("="));
+                case ',' -> tokens.add(Token.simple(","));
+                case ';' -> tokens.add(Token.simple("Semicolon"));
+                case '+' -> tokens.add(Token.simple("+"));
+                case '-' -> tokens.add(Token.simple("-"));
+                case '*' -> tokens.add(Token.simple("*"));
+                case '/' -> tokens.add(Token.simple("/"));
+                case '(' -> tokens.add(Token.simple("("));
+                case ')' -> tokens.add(Token.simple(")"));
+                default -> {
+                    // Ignore unexpected characters in lab1 grammar.
+                }
+            }
+
+            index++;
+        }
+
+        tokens.add(Token.eof());
     }
 
     /**
@@ -53,7 +114,7 @@ public class LexicalAnalyzer {
         // 词法分析过程可以使用 Stream 或 Iterator 实现按需分析
         // 亦可以直接分析完整个文件
         // 总之实现过程能转化为一列表即可
-        throw new NotImplementedException();
+        return tokens;
     }
 
     public void dumpTokens(String path) {
@@ -62,6 +123,17 @@ public class LexicalAnalyzer {
             StreamSupport.stream(getTokens().spliterator(), false).map(Token::toString).toList()
         );
     }
+
+    private boolean isIdentifierStart(char ch) {
+        return Character.isLetter(ch) || ch == '_';
+    }
+
+    private boolean isIdentifierPart(char ch) {
+        return Character.isLetterOrDigit(ch) || ch == '_';
+    }
+
+    private String sourceCode = "";
+    private List<Token> tokens = new ArrayList<>();
 
 
 }
